5c9b5d5dbf91ffcbd1fea264c3b34562968d61c5
Bo. <58321028+zatserkovnyy@users.noreply.github.com>
2026-09-09 19:07:54 +0300

    Update sso.lua

--- sso.lua
+++ sso.lua
@@ -2,37 +2,39 @@
 -- Script: sso.lua
 -- Description: Toggle ASS/SSA Subtitle Style Override (SSO) for mpv
 -- Author: Boris Zatserkovnyy
--- Version: 1.0.1
+-- Version: 1.0.2
 -- GitHub: https://github.com/zatserkovnyy/mpv-sso
 -- =======================================================
 
-function toggle_sub_style()
-    local track_list = mp.get_property_native("track-list")
-    local sub_format = ""
-
-    for _, track in ipairs(track_list) do
+local function get_active_sub_format()
+    for _, track in ipairs(mp.get_property_native("track-list") or {}) do
         if track.type == "sub" and track.selected then
-            sub_format = track.codec
-            break
+            return track.codec
         end
     end
+    return nil
+end
 
-    if sub_format == "ass" or sub_format == "ssa" then
-        local current_override = mp.get_property("sub-ass-override")
+function toggle_sub_style()
+    local sub_format = get_active_sub_format()
 
-        if current_override ~= "force" then
-            mp.set_property("sub-ass-override", "force")
-            mp.set_property("sub-ass-force-style", "Bold=1,Fontsize=36")
-            mp.set_property("sub-use-margins", "yes")
-            mp.set_property("sub-pos", "98")
-            mp.osd_message("ASS Override: on")
-        else
-            mp.set_property("sub-ass-override", "no")
-            mp.set_property("sub-ass-force-style", "")
-            mp.osd_message("ASS Override: off")
-        end
-    else
+    if sub_format ~= "ass" and sub_format ~= "ssa" then
         mp.osd_message([[¯\_(ツ)_/¯]])
+        return
+    end
+
+    local is_override_active = mp.get_property("sub-ass-override") == "force"
+
+    if is_override_active then
+        mp.set_property("sub-ass-override", "no")
+        mp.set_property("sub-ass-force-style", "")
+        mp.osd_message("ASS Override: off")
+    else
+        mp.set_property("sub-ass-override", "force")
+        mp.set_property("sub-ass-force-style", "Bold=1,Fontsize=36")
+        mp.set_property("sub-use-margins", "yes")
+        mp.set_property("sub-pos", "98")
+        mp.osd_message("ASS Override: on")
     end
 end
 
